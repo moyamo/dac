03a0755004e6198f660b43a4c7a4c749173f46eb
Yaseen Mowzer <yaseen@mowzer.co.za>
2023-08-21 15:49:49 +0200

    fix(logo): not updating public/logo(192|512).png
    `logo/README.sh` is updating `public/favicon.ico`, but we are forgetting
    to also update `public/logo192.png` and `public/logo512.png`.
    
    Update those files too so that the right logo is shown in all
    places. (I'm not entirely sure where `public/logo(192|512).png` are
    used.

--- logo/README.sh
+++ logo/README.sh
@@ -9,6 +9,9 @@ pdflatex logo.tex
 # See https://stackoverflow.com/questions/12179342/imagemagick-extend-canvas-with-transparent-background
 FAVICON_INPUT="logo.pdf"
 FAVICON_OUTPUT="../public/favicon.ico"
+LOGO192_OUTPUT="../public/logo192.png"
+LOGO512_OUTPUT="../public/logo512.png"
+
 convert -density 10080 "$FAVICON_INPUT" \
       -gravity center -background none \
       \( -clone 0 -resize 16x16 -extent 16x16 \) \
@@ -16,3 +19,13 @@ convert -density 10080 "$FAVICON_INPUT" \
       \( -clone 0 -resize 48x48 -extent 48x48 \) \
       \( -clone 0 -resize 64x64 -extent 64x64 \) \
       -delete 0 -alpha background -colors 256 "$FAVICON_OUTPUT"
+
+convert -density 10080 "$FAVICON_INPUT" \
+      -gravity center -background none \
+      -resize 192x192 -extent 192x192  \
+      -alpha background -colors 256 "$LOGO192_OUTPUT"
+
+convert -density 10080 "$FAVICON_INPUT" \
+      -gravity center -background none \
+      -resize 512x512 -extent 512x512  \
+      -alpha background -colors 256 "$LOGO512_OUTPUT"
