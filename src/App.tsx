moyamo/dac

commit 55f8fe8c3af3393253f024473a80ec79b4a191b6

feat: close funding after funding deadline and enable /refund
- WIP: I should disable refund if the project has met it's goal.

When the project deadline has passed we don't allow further funding. If
it has not reached the goal then we then enable a `/refund` route. We
can manually refund a single person with `POST /refund`. Once there are
no more people to refund `POST /refund` returns 404.

src/App.tsx
@@ -7,9 +7,10 @@ import type {
   OnClickActions,
 } from "@paypal/paypal-js/types/components/buttons";
 import React from "react";
+import Countdown from "react-countdown";
 import "./App.css";
-import type { CreateOrderResponse, Order } from "./worker";
-import { getInvalidAmountError } from "./common";
+import type { CounterResponse, CreateOrderResponse, Order } from "./worker";
+import { getInvalidAmountError, hasFundingDeadlinePassed } from "./common";
 
 export const WORKER_URL =
   process.env.REACT_APP_WORKER_URL || "http://localhost:8787";
@@ -38,6 +39,9 @@ function App(props: AppProps) {
   const [refunded, setRefunded] = React.useState(false);
   const [amountRef, setAmount] = useStateRef(19);
   const [progress, setProgress] = React.useState(-1);
+  const [fundingGoal, setFundingGoal] = React.useState(-1);
+  const [fundingDeadline, setFundingDeadline] = React.useState("");
+
   const [orders, setOrders] = React.useState<Order[]>([]);
 
   React.useEffect(() => {
@@ -52,9 +56,11 @@ function App(props: AppProps) {
   React.useEffect(() => {
     void (async () => {
       const count = await fetch(WORKER_URL + "/counter");
-      const response = await count.json<{ amount: number; orders: Order[] }>();
+      const response = await count.json<CounterResponse>();
       setProgress(response.amount);
       setOrders(response.orders);
+      setFundingGoal(response.fundingGoal);
+      setFundingDeadline(response.fundingDeadline);
     })();
   }, [funded]);
 
@@ -69,75 +75,88 @@ function App(props: AppProps) {
         "Sorry, the project did not reach the goal. The money is been refunded"
       ) : (
         <>
+          <FundingTimer deadline={fundingDeadline} />
           <form>
-            <label htmlFor="amount">Amount</label>
-            <input
-              type="number"
-              id="amount"
-              name="amount"
-              min="5"
-              max="500"
-              value={amountRef.current}
-              step="1"
-              onChange={(e) => setAmount(Number(e.target.value))}
+            {hasFundingDeadlinePassed(fundingDeadline) ? null : (
+              <>
+                <label htmlFor="amount">Amount</label>
+                <input
+                  type="number"
+                  id="amount"
+                  name="amount"
+                  min="5"
+                  max="500"
+                  value={amountRef.current}
+                  step="1"
+                  onChange={(e) => setAmount(Number(e.target.value))}
+                />
+                <p>
+                  {getInvalidAmountError(amountRef.current) ||
+                    `Thanks for pledging $${
+                      amountRef.current
+                    }! If we do not reach our goal you will get a $${(
+                      amountRef.current * 1.2
+                    ).toFixed(2)} refund!`}
+                </p>
+              </>
+            )}
+            <FundingProgressBar
+              funded={funded}
+              progress={progress}
+              goal={fundingGoal}
             />
-
-            <p>
-              {getInvalidAmountError(amountRef.current) ||
-                `Thanks for pledging $${
-                  amountRef.current
-                }! If we do not reach our goal you will get a $${(
-                  amountRef.current * 1.2
-                ).toFixed(2)} refund!`}
-            </p>
-            <FundingProgressBar funded={funded} progress={progress} />
-            <PaypalButtons
-              fundingSource={
-                /* Don't allow weird sources, because I may Paypal the money back */
-                FUNDING.PAYPAL
-              }
-              onClick={(_data: unknown, actions: OnClickActions) => {
-                const amount = amountRef.current;
-                if (getInvalidAmountError(amount) == null) {
-                  return actions.resolve();
-                } else {
-                  return actions.reject();
-                }
-              }}
-              createOrder={async (
-                _data: CreateOrderData,
-                _actions: CreateOrderActions
-              ) => {
-                const response = await fetch(WORKER_URL + "/contract", {
-                  method: "POST",
-                  body: JSON.stringify({ amount: amountRef.current }),
-                });
-                const responseJson: CreateOrderResponse = await response.json();
-                console.dir(responseJson);
-                return responseJson.id;
-              }}
-              onApprove={async (
-                data: OnApproveData,
-                actions: OnApproveActions
-              ) => {
-                console.log("order approved");
-                console.dir(data);
-                console.log("actions");
-                console.dir(actions);
-                const response = await fetch(
-                  WORKER_URL + "/contract/" + data.orderID,
-                  {
-                    method: "PATCH",
+            {hasFundingDeadlinePassed(fundingDeadline) ? null : (
+              <>
+                <PaypalButtons
+                  fundingSource={
+                    /* Don't allow weird sources, because I may Paypal the money back */
+                    FUNDING.PAYPAL
                   }
-                );
-                console.log("got response");
-                console.dir(response);
-                const responseJson = await response.json();
-                console.log("responseJson");
-                console.dir(responseJson);
-                setFunded(true);
-              }}
-            />
+                  onClick={(_data: unknown, actions: OnClickActions) => {
+                    const amount = amountRef.current;
+                    if (getInvalidAmountError(amount) == null) {
+                      return actions.resolve();
+                    } else {
+                      return actions.reject();
+                    }
+                  }}
+                  createOrder={async (
+                    _data: CreateOrderData,
+                    _actions: CreateOrderActions
+                  ) => {
+                    const response = await fetch(WORKER_URL + "/contract", {
+                      method: "POST",
+                      body: JSON.stringify({ amount: amountRef.current }),
+                    });
+                    const responseJson: CreateOrderResponse =
+                      await response.json();
+                    console.dir(responseJson);
+                    return responseJson.id;
+                  }}
+                  onApprove={async (
+                    data: OnApproveData,
+                    actions: OnApproveActions
+                  ) => {
+                    console.log("order approved");
+                    console.dir(data);
+                    console.log("actions");
+                    console.dir(actions);
+                    const response = await fetch(
+                      WORKER_URL + "/contract/" + data.orderID,
+                      {
+                        method: "PATCH",
+                      }
+                    );
+                    console.log("got response");
+                    console.dir(response);
+                    const responseJson = await response.json();
+                    console.log("responseJson");
+                    console.dir(responseJson);
+                    setFunded(true);
+                  }}
+                />
+              </>
+            )}
           </form>
           <h3>Funders</h3>
           <FunderTable orders={orders} />
@@ -150,19 +169,52 @@ function App(props: AppProps) {
 export type FundingProgressBarProps = {
   funded: boolean;
   progress: number;
+  goal: number;
 };
 
 export function FundingProgressBar(props: FundingProgressBarProps) {
-  const { funded, progress } = props;
+  const { funded, progress, goal } = props;
   return progress == -1 ? (
     <span>Loading...</span>
   ) : (
     <>
-      <progress role="progressbar" value={progress} max={833} />
-      {`$${progress} / $${833} funded!`}
+      <progress role="progressbar" value={progress} max={goal} />
+      {`$${progress} / $${goal} funded!`}
       {funded ? " Thank you!" : null}
     </>
   );
+}
+
+export type FundingTimerProps = {
+  deadline: string;
+};
+
+export function FundingTimer(props: FundingTimerProps) {
+  const { deadline } = props;
+  if (deadline == "") {
+    return <span>Loading deadline...</span>;
+  } else if (hasFundingDeadlinePassed(deadline)) {
+    return (
+      <span>
+        Funding closed on {formatTime(deadline)}. No more funds are being
+        accepted.
+      </span>
+    );
+  } else {
+    // Something is going weird with the modules in this pacakge which causes
+    // the Coundown function to be on the default property instead of being
+    // Countdown itself
+    const CountdownDefault =
+      "default" in Countdown
+        ? (Countdown.default as React.FunctionComponent)
+        : Countdown;
+    return (
+      <span>
+        Funding closing on {formatTime(deadline)}.{" "}
+        <CountdownDefault date={new Date(deadline)} />{" "}
+      </span>
+    );
+  }
 }
 
 export type FunderTableProps = {

src/App.test.tsx
@@ -10,6 +10,7 @@ import App, {
   WORKER_URL,
   FunderTable,
   formatTime,
+  FundingTimer,
 } from "./App";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
@@ -18,15 +19,26 @@ import { PayPalButtonsComponentProps } from "@paypal/react-paypal-js";
 let counter = 0;
 let refunded = false;
 let pendingAmount: number | null = null;
+let fundingDeadline = "2023-01-01T01:01:01Z";
 beforeEach(() => {
   counter = 0;
   refunded = false;
   pendingAmount = null;
+  const future = new Date();
+  future.setHours(future.getHours() + 24);
+  fundingDeadline = future.toISOString();
 });
 
 const server = setupServer(
   rest.get(WORKER_URL + "/counter", (_req, res, ctx) => {
-    return res(ctx.json({ amount: counter, orders: [] }));
+    return res(
+      ctx.json({
+        amount: counter,
+        orders: [],
+        fundingGoal: 507,
+        fundingDeadline: fundingDeadline,
+      })
+    );
   }),
   rest.get(WORKER_URL + "/refund", (_req, res, ctx) => {
     return refunded ? res() : res(ctx.status(404));
@@ -186,19 +198,52 @@ test("Custom amount of $32 dollars accepted", async () => {
   expect(counter).toBe(32);
 });
 
+test("Funding deadline passed", async () => {
+  fundingDeadline = "2023-01-01T01:03:00Z";
+  render(<App PaypalButtons={MockPaypalButtons} />);
+  expect(await screen.findByText(/Funding closed/i)).toBeInTheDocument();
+  expect(screen.queryByText("PayPal")).not.toBeInTheDocument();
+  expect(screen.queryByLabelText("Amount")).not.toBeInTheDocument();
+});
+
+test("Funding deadline shown", async () => {
+  fundingDeadline = new Date(2023, 0, 1, 18, 1, 0).toISOString();
+  render(<App PaypalButtons={MockPaypalButtons} />);
+  expect(await screen.findByText(/2023-01-01 18:01/i)).toBeInTheDocument();
+});
+
+test("Funding count-down shown", async () => {
+  const future = new Date();
+  future.setDate(future.getDate() + 1);
+  future.setHours(future.getHours() + 2);
+  future.setMinutes(future.getMinutes() + 3);
+  future.setSeconds(future.getSeconds() + 4);
+  fundingDeadline = future.toISOString();
+
+  render(<App PaypalButtons={MockPaypalButtons} />);
+  expect(await screen.findByText("01:02:03:04")).toBeInTheDocument();
+});
+
 test("FundingProgressBar", async () => {
   const { rerender } = render(
-    <FundingProgressBar funded={false} progress={3 * 19} />
+    <FundingProgressBar funded={false} progress={3 * 19} goal={203} />
   );
   const progressbar = await screen.findByRole("progressbar");
-  await waitFor(() =>
-    expect(progressbar).toHaveAttribute("value", `${19 * 3}`)
-  );
+  await waitFor(() => {
+    expect(progressbar).toHaveAttribute("value", `${19 * 3}`);
+    expect(progressbar).toHaveAttribute("max", "203");
+  });
 
-  rerender(<FundingProgressBar funded={true} progress={19 * 4} />);
-  await waitFor(() =>
-    expect(progressbar).toHaveAttribute("value", `${19 * 4}`)
-  );
+  rerender(<FundingProgressBar funded={true} progress={19 * 4} goal={203} />);
+  await waitFor(() => {
+    expect(progressbar).toHaveAttribute("value", `${19 * 4}`);
+    expect(progressbar).toHaveAttribute("max", "203");
+  });
+});
+
+test("Funding Timer Loading", async () => {
+  render(<FundingTimer deadline="" />);
+  expect(await screen.findByText(/Loading/i)).toBeInTheDocument();
 });
 
 test("Table of Funders", async () => {

dev.vars.exmaple
@@ -3,3 +3,7 @@ PAYPAL_APP_SECRET=blahblahblah
 # Since the frontend and backend are on different origins,
 # this is used by the backend to CORS-allow the frontend.
 FRONTEND_URL=http://localhost:3000
+# The ISO time of when funding closes
+FUNDING_DEADLINE=2030-06-18T17:00:00Z
+# The amount of USD needed to reach the goal
+FUNDING_GOAL=850

src/common.ts
@@ -7,3 +7,7 @@ export function getInvalidAmountError(amount: number): string | null {
     return null;
   }
 }
+
+export function hasFundingDeadlinePassed(fundingDeadline: string): boolean {
+  return fundingDeadline < new Date().toISOString();
+}

src/paypal.reverse.spec.ts
@@ -418,8 +418,127 @@ test("capture payment", async () => {
   const capture = body2.purchase_units[0].payments.captures[0];
   expect(capture.amount.currency_code).toBe("USD");
   expect(capture.amount.value).toBe(amountUsd);
+  expect(capture.id.length).toBeGreaterThanOrEqual(1);
 }, 30000 /* ms. Increase timout it since we open chrome which could
- take a while. */);
+            take a while. */);
+
+test("Refund Paypal payment", async () => {
+  const accessToken = await generateAccessToken();
+  const amountUsd = "7.00";
+  // Create the order on the backend
+  const response = await fetch(`${baseURL.sandbox}/v2/checkout/orders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: amountUsd,
+          },
+        },
+      ],
+    }),
+  });
+  const body: Paypal.CreateOrderResponse = await response.json();
+  const orderId = body.id;
+
+  // Approve the order as the user
+  if (TEST_WHAT == "SPEC") {
+    const approveLink = body.links.filter((link) => link.rel == "approve")[0];
+    await (async () => {
+      // Doesn't seem to work in headless mode. Fix later.
+      const browser = await puppeteer.launch({ headless: false });
+      const page = await browser.newPage();
+      await page.goto(approveLink.href);
+      await page.type("input[type=email]", PAYPAL_SANDBOX_EMAIL_1, {
+        delay: 20,
+      });
+      await page.click("button[type=submit]");
+      await page.waitForSelector("input[type=password]", { visible: true });
+      await page.type("input[type=password]", PAYPAL_SANDBOX_EMAIL_1_PASSWORD, {
+        delay: 20,
+      });
+      await page.click("#btnLogin");
+      await page.waitForSelector("#payment-submit-btn", { visible: true });
+      await page.click("#payment-submit-btn");
+      await page.waitForNavigation();
+      await browser.close();
+    })();
+  } else {
+    // I don't want to mock the whole paypal UI, so let's just make a custom request.
+
+    await fetch(`${baseURL.sandbox}/mock/approve`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        orderId: orderId,
+        email: PAYPAL_SANDBOX_EMAIL_1,
+        givenName: PAYPAL_SANDBOX_EMAIL_1_GIVEN_NAME,
+        surname: PAYPAL_SANDBOX_EMAIL_1_SURNAME,
+      }),
+    });
+  }
+
+  // Capture the order.
+  const captureUrl = `${baseURL.sandbox}/v2/checkout/orders/${orderId}/capture`;
+  const response2 = await fetch(captureUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const body2 = await response2.json<Paypal.CapturePaymentResponse>();
+  const captureId = body2.purchase_units[0].payments.captures[0].id;
+
+  // Finally, refund the order
+  const refundUrl = `${baseURL.sandbox}/v2/payments/captures/${captureId}/refund`;
+  const response3 = await fetch(refundUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  expect(response3.status).toBe(201);
+  const body3 = await response3.json();
+  expect(body3).toMatchInlineSnapshot(
+    {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      id: expect.stringMatching("[A-Z0-9]+"),
+      links: new Array(2).fill({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        href: expect.stringMatching("https://(www|api).sandbox.paypal.com/.*"),
+      }),
+    },
+    `
+    Object {
+      "id": StringMatching /\\[A-Z0-9\\]\\+/,
+      "links": Array [
+        Object {
+          "href": StringMatching /https:\\\\/\\\\/\\(www\\|api\\)\\.sandbox\\.paypal\\.com\\\\/\\.\\*/,
+          "method": "GET",
+          "rel": "self",
+        },
+        Object {
+          "href": StringMatching /https:\\\\/\\\\/\\(www\\|api\\)\\.sandbox\\.paypal\\.com\\\\/\\.\\*/,
+          "method": "GET",
+          "rel": "up",
+        },
+      ],
+      "status": "COMPLETED",
+    }
+  `
+  );
+}, 30000 /* ms. Increase timout it since we open chrome which could
+            take a while. */);
 
 type PaypalV1Oauth2Token = {
   access_token: string;

src/paypalMock.ts
@@ -13,6 +13,7 @@ const closure = {
   amountUsd: "",
   givenName: "",
   surname: "",
+  orderCount: 0,
 };
 
 export default function () {
@@ -25,6 +26,7 @@ export default function () {
   closure.amountUsd = "";
   closure.givenName = "";
   closure.surname = "";
+  closure.orderCount = 0;
 
   paypalSandbox
     .intercept({ method: "POST", path: "/v1/oauth2/token" })
@@ -129,6 +131,7 @@ export default function () {
       })
     );
   const orderId = "ABC0123456789";
+  const captureId = "XZY98765421";
   paypalSandbox
     .intercept({ method: "POST", path: "/v2/checkout/orders" })
     .reply(
@@ -136,7 +139,8 @@ export default function () {
         if (typeof body != "string") {
           return { statusCode: 400 };
         }
-        const id = orderId;
+        const id = `${orderId}${closure.orderCount}`;
+        closure.orderCount += 1;
         const bodyJson = JSON.parse(body) as {
           purchase_units: Array<{ amount: { value: string } }>;
         };
@@ -198,44 +202,92 @@ export default function () {
       }
     });
 
+  function capture(count: number) {
+    return withAuthorization(() => {
+      console.log("Capture Amount USD", closure.amountUsd);
+      return {
+        statusCode: 201,
+        data: JSON.stringify({
+          payment_source: {
+            paypal: {
+              email_address: closure.email,
+              name: {
+                given_name: closure.givenName,
+                surname: closure.surname,
+              },
+            },
+          },
+          purchase_units: [
+            {
+              payments: {
+                captures: [
+                  {
+                    id: `${captureId}${count}`,
+                    amount: {
+                      currency_code: "USD",
+                      value: closure.amountUsd,
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        }),
+      };
+    });
+  }
   paypalSandbox
     .intercept({
       method: "POST",
-      path: `/v2/checkout/orders/${orderId}/capture`,
+      path: `/v2/checkout/orders/${orderId}0/capture`,
     })
-    .reply(
-      withAuthorization(() => {
-        console.log("Capture Amount USD", closure.amountUsd);
-        return {
-          statusCode: 201,
-          data: JSON.stringify({
-            payment_source: {
-              paypal: {
-                email_address: closure.email,
-                name: {
-                  given_name: closure.givenName,
-                  surname: closure.surname,
-                },
-              },
+    .reply(capture(0));
+
+  paypalSandbox
+    .intercept({
+      method: "POST",
+      path: `/v2/checkout/orders/${orderId}1/capture`,
+    })
+    .reply(capture(0));
+
+  function refund(count: number) {
+    return withAuthorization(() => {
+      const refundId = `REFUNDID${count}`;
+      return {
+        statusCode: 201,
+        data: JSON.stringify({
+          id: refundId,
+          status: "COMPLETED",
+          links: [
+            {
+              href: `https://api.sandbox.paypal.com/v2/payments/refunds/${refundId}`,
+              method: "GET",
+              rel: "self",
             },
-            purchase_units: [
-              {
-                payments: {
-                  captures: [
-                    {
-                      amount: {
-                        currency_code: "USD",
-                        value: closure.amountUsd,
-                      },
-                    },
-                  ],
-                },
-              },
-            ],
-          }),
-        };
-      })
-    );
+            {
+              href: `https://api.sandbox.paypal.com/v2/payments/captures/${captureId}${count}`,
+              method: "GET",
+              rel: "up",
+            },
+          ],
+        }),
+      };
+    });
+  }
+
+  paypalSandbox
+    .intercept({
+      method: "POST",
+      path: `/v2/payments/captures/${captureId}0/refund`,
+    })
+    .reply(refund(0));
+
+  paypalSandbox
+    .intercept({
+      method: "POST",
+      path: `/v2/payments/captures/${captureId}1/refund`,
+    })
+    .reply(refund(1));
 }
 
 /** Get's authorization header from mocked headers */

src/paypalTypes.ts
@@ -84,3 +84,9 @@ export type CapturePaymentResponse = {
   }>;
   status: string;
 };
+
+export type RefundCaptureResponse = {
+  id: string;
+  status: string;
+  links: Link[];
+};

commit 04058a8ca4affc44b9182f98cc606dd21ea4c92f

fix(App.tsx): fetching /refund incorrectly
Commit 55f8fe8 ("feat: close funding after funding deadline and enable
/refund") changed the API of /refund. However, I forgot to update both
App.tsx and the mock in App.test.tsx to reflect the change in API. This
bug does not cause any visible issue. I just wanted to remove this
vestigial code.

Fixes: 55f8fe8 ("feat: close funding after funding deadline and enable /refund")

src/App.tsx
@@ -43,22 +43,12 @@ export type AppProps = {
 function App(props: AppProps) {
   const { PaypalButtons, headerParenthesis } = props;
   const [funded, setFunded] = React.useState(false);
-  const [refunded, setRefunded] = React.useState(false);
   const [amountRef, setAmount] = useStateRef(89);
   const [progress, setProgress] = React.useState(-1);
   const [fundingGoal, setFundingGoal] = React.useState(-1);
   const [fundingDeadline, setFundingDeadline] = React.useState("");
 
   const [orders, setOrders] = React.useState<Order[]>([]);
-
-  React.useEffect(() => {
-    void (async () => {
-      const refund = await fetch(WORKER_URL + "/refund");
-      if (refund.ok) {
-        setRefunded(true);
-      }
-    })();
-  }, [funded]);
 
   React.useEffect(() => {
     void (async () => {
@@ -79,9 +69,7 @@ function App(props: AppProps) {
         }`}</h1>
       </header>
 
-      {refunded ? (
-        "Sorry, the project did not reach the goal. The money is been refunded"
-      ) : (
+      {
         <>
           <form>
             <h2>
@@ -310,7 +298,7 @@ function App(props: AppProps) {
           <h2>Funders</h2>
           <FunderTable orders={orders} />
         </>
-      )}
+      }
     </>
   );
 }

src/App.test.tsx
@@ -19,13 +19,11 @@ import { PayPalButtonsComponentProps } from "@paypal/react-paypal-js";
 import type { Bonus } from "./worker";
 
 let counter = 0;
-let refunded = false;
 let pendingAmount: number | null = null;
 let fundingDeadline = "2023-01-01T01:01:01Z";
 let bonuses: Record<string, Bonus> = {};
 beforeEach(() => {
   counter = 0;
-  refunded = false;
   pendingAmount = null;
   const future = new Date();
   future.setHours(future.getHours() + 24);
@@ -46,9 +44,6 @@ const server = setupServer(
         fundingDeadline: fundingDeadline,
       })
     );
-  }),
-  rest.get(WORKER_URL + "/refund", (_req, res, ctx) => {
-    return refunded ? res() : res(ctx.status(404));
   }),
   rest.post(WORKER_URL + "/contract", async (req, res, ctx) => {
     const jsonBody = await req.json<{ amount: number }>();
@@ -137,15 +132,6 @@ function MockPaypalButtons(props: PayPalButtonsComponentProps) {
     </button>
   );
 }
-
-test("App refunded", async () => {
-  refunded = true;
-  render(<App PaypalButtons={MockPaypalButtons} />);
-  const refundText = await screen.findByText(
-    /the project did not reach the goal/i
-  );
-  expect(refundText).toBeInTheDocument();
-});
 
 test("App in-progress", async () => {
   render(<App PaypalButtons={MockPaypalButtons} />);